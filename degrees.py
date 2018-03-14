e108c614f22709d3ae7b6e912a3431b1d171aeea
drawjk705 <Drawjk705@gmail.com>
2018-03-13 21:42:58 -0400

    wrote get_id_from_name() function

--- degrees.py
+++ degrees.py
@@ -7,20 +7,34 @@ Documentation: https://developers.themoviedb.org/3/getting-started/introduction
 
 import requests
 import urllib.parse as ul
+import time
+import json
 from pprint import pprint
 
 key = "api_key=665e86f05c67c2031783a1a12386ece4"
 endpoint = "https://api.themoviedb.org/3/"
 
-# map from actor's ID to his/her name
-id_to_name = {}
+def check_if_ready():
+    """
+    function to make sure that API is ready to process
+    a new request
+    :param count:
+    :return:
+    """
 
-# map from movie's ID to title
-id_to_movie = {}
+    header = requests.get(endpoint).headers
 
-# map from actor to all of his/her costars,
-# as well as in which movie they costarred
-actors_to_costars = {}
+    while True:
+        if "Retry-After" in header:
+            print("Not ready")
+            wait = header.get("Retry-After")
+            time.sleep(int(wait) + 1)
+        else:
+            count = header.get("X-RateLimit-Remaining")
+            return count
+        header = requests.get(endpoint).headers
+
+
 
 def get_id_from_name(name):
     """
@@ -39,4 +53,57 @@ def get_id_from_name(name):
 
     return id
 
-get_id_from_name("Liam Neeson")
+
+def get_actor_filmography(actor_id):
+
+    response = requests.get(endpoint + "person/" + str(actor_id) + "/movie_credits?" + key).json()
+
+    filmog = response.get("cast")
+
+    films = {}
+
+    for film in filmog:
+        films.update({film.get("id"): film.get("title")})
+
+    pprint(films)
+
+# 50463
+
+def get_cast(film_id):
+
+    response = requests.get(endpoint + "movie/" + str(film_id) + "/credits?" + key).json()
+
+    cast = response.get("cast")
+
+    actors = {}
+
+    for member in cast:
+        if "character" in member:
+            actor_name = member.get("name")
+            actor_id = member.get("id")
+            actors.update({actor_id: actor_name})
+
+    pprint(actors)
+
+
+# map from actor's ID to his/her name
+id_to_name = {}
+
+# map from movie's ID to title
+id_to_movie = {}
+
+# map from actor to all of his/her costars,
+# as well as in which movie they costarred
+actors_to_costars = {}
+
+
+def get_actor_relation(actor1, actor2):
+
+    id1 = get_id_from_name(actor1)
+    id2 = get_id_from_name(actor2)
+
+    pass
+
+check_if_ready()
+# get_actor_filmography(get_id_from_name("Brad Pitt"))
+get_cast(60308)
