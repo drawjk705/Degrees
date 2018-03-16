b8274055ca6157f4834b1cbb89d98553e2c9c34a
drawjk705 <Drawjk705@gmail.com>
2018-03-16 18:08:40 -0400

    - created cocast.txt and id_to_actor.txt files. - also worked on get_connection()

--- degrees.py
+++ degrees.py
@@ -4,16 +4,20 @@ Written by Joel Krim
 This project uses The Movie Database (TMDb) REST API
 Documentation: https://developers.themoviedb.org/3/getting-started/introduction
 """
+from webbrowser import get
 
 import requests
 import urllib.parse as ul
 import time
 import json
+from copy import deepcopy
 from pprint import pprint
 
 key = "api_key=665e86f05c67c2031783a1a12386ece4"
 endpoint = "https://api.themoviedb.org/3/"
 status = "status_message"
+cocast = "cocast.txt"
+id_to_actor = "id_to_actor.txt"
 
 def check_if_ready():
     """
@@ -92,25 +96,23 @@ def get_cast(film_id):
 
     return actors
 
-
-# map from actor's ID to his/her name
-id_to_name = {}
-
 # map from movie's ID to title
-id_to_movie = {}
-
-# map from actor to all of his/her costars,
-# as well as in which movie they costarred
-actors_to_costars = {}
+# id_to_movie = {}
 
 
-def get_costars(actor1):
+def get_costars(actor_id):
 
-    id1 = get_id_from_name(actor1)
+    # id1 = get_id_from_name(actor1)
 
-    filmog = get_actor_filmography(id1)
+    filmog = get_actor_filmography(actor_id)
 
-    costars = []
+    costars = {}
+
+    id_to_name = {}
+    try:
+        id_to_name = extract_json_from_file(id_to_actor)
+    except:
+        pass
 
     for film in filmog:
         # get cast of films
@@ -121,42 +123,65 @@ def get_costars(actor1):
             # add to overall dict if need to
             if actor not in id_to_name:
                 id_to_name.update(cast)
-            if str(id1) != str(actor):
-                costars.append(({actor: cast.get(actor)}, filmog.get(film)))
-    actors_to_costars.update({id1: costars})
+            if str(actor_id) != str(actor):
+                costars.update({actor: cast.get(actor)})
+                id_to_name.update({actor: cast.get(actor)})
+    data = {}
+    try:
+        data = extract_json_from_file(cocast)
+    except:
+        pass
+    data.update({actor_id: costars})
+    write_out(data, cocast)
+    write_out(id_to_name, id_to_actor)
     return costars
 
 
-def check_connection(actor1, actor2, path=[]):
+# add functionality to see the movies the 2 were in together
+def check_connection(actor1, actor2, pass_num=0, path=[]):
 
     path.append(actor1)
 
-    print(path)
-    if actor1 == actor2:
-        print("woo")
-    if len(path) == 6:
-        return 0
+    data = {}
+    try:
+        data = extract_json_from_file(cocast)
+    except:
+        pass
+    # pprint(data)
 
+    id1 = get_id_from_name(actor1)
     id2 = get_id_from_name(actor2)
+    pprint(id2)
 
-    costars = get_costars(actor1)
-    for costar in costars:
-        for key in costar[0]:
-            actor = costar[0].get(key)
-            if actor not in path:
-                check_connection(actor, actor2, path)
+    costars = {}
+    if str(id1) in data:
+        costars = data.get(str(id1))
+        pprint(costars)
+    else:
+        costars = get_costars(id1)
+    if str(id2) in costars:
+        path.append(actor2)
+        print("HOORAY: ", path)
+        return True
+
+    for i in range(6 - pass_num):
+        for costar in costars:
+            if costars.get(costar) not in path:
+                if check_connection(costars.get(costar), actor2, pass_num + 1, deepcopy(path)) is True:
+                    return True
 
 
 
 
+def extract_json_from_file(filename):
+    return json.load(open(filename))
 
-def write_out():
-    with open("actors.txt", "w") as outfile:
-        json.dump(actors_to_costars, outfile)
 
-#
-# check_if_ready()
-# # get_actor_filmography(get_id_from_name("Brad Pitt"))
-# get_costars("Brad Pitt")
+def write_out(data, filename):
+    with open(filename, "w") as outfile:
+        json.dump(data, outfile)
 
-check_connection("Brad Pitt", "Margot Robbie")
+###############################################################
+###############################################################
+
+check_connection("Margot Robbie", "Joel Edgerton")
